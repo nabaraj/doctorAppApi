574fb2d7f3e987b0729e6cffbd09565954aeca90
Nabaraj Saha <nabara2k@gmail.com>
2020-10-28 00:37:11 +0530

    added logout rout

--- routes/doctor.js
+++ routes/doctor.js
@@ -1,8 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-let {generateAccessToken} = require('../utils/utility');
+let { generateAccessToken } = require("../utils/utility");
+let { authorization } = require("./../utils/utility");
 // /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
@@ -10,14 +11,13 @@ let {generateAccessToken} = require('../utils/utility');
 
 // module.exports = router;
 
-
 const { Doctor } = require("../models/doctor_model");
 const mongoose = require("mongoose");
 
 router.post("/registration", async (req, res) => {
   let user = await Doctor.findOne({
     email: req.body.email,
-    userName: req.body.userName
+    userName: req.body.userName,
   });
   if (user) return res.status(400).send("Doctor's entry already exists");
   else
@@ -26,59 +26,58 @@ router.post("/registration", async (req, res) => {
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
-      department:req.body.department,
-      dob:req.body.dob || new Date(),
-      specialization:req.body.specialization,
-      token:'',
-      phone:req.body.phone,
-      roles:req.body.roles,
-      initials:req.body.initials
+      department: req.body.department,
+      dob: req.body.dob || new Date(),
+      specialization: req.body.specialization,
+      token: "",
+      phone: req.body.phone,
+      roles: req.body.roles,
+      initials: req.body.initials,
     });
   const salt = await bcrypt.genSalt(3);
   console.log("salt", salt);
   user.password = await bcrypt.hash(user.password, salt);
-  console.log("user ",user);
+  console.log("user ", user);
   user = await user.save();
   res.send({
     userName: user.userName,
     name: user.name,
-    email: user.email
+    email: user.email,
   });
 });
-router.post('/login', async(req, res)=>{
-  console.log("#### ",req.body.email, req.body.password);
-  
+
+router.post("/login", async (req, res) => {
+  console.log("#### ", req.body.email, req.body.password);
+
   let user = await Doctor.findOne({
-    userName: req.body.userName
+    userName: req.body.userName,
   });
-  console.log("####$$ ",user);
+  console.log("####$$ ", user);
   if (user) {
     // return res.status(200).send("Login Successfull");
-    console.log('if');
+    console.log("if");
     const match = await bcrypt.compare(req.body.password, user.password);
-      console.log('isMatch ',match);
-      
+    console.log("isMatch ", match);
 
-      if(match){
-        let userObj = {userName:req.body.userName};
-        let token = generateAccessToken(userObj);
-        console.log("**** ",token);
-       let filter= {_id:user._id};
-       const update = {'token':token};
+    if (match) {
+      let userObj = { userName: req.body.userName };
+      let token = generateAccessToken(userObj);
+      console.log("**** ", token);
+      let filter = { _id: user._id };
+      const update = { token: token };
 
-       let userData = await Doctor.update(filter, update)
-        // jwt.sign({ email: req.body.email }, function(err, token) {
-          console.log("userData ",userData);
-        // });
-        // await user.save();
-        let updatedUser = await Doctor.findOne(filter)
-        res.status(200).json(updatedUser);
-      }else{
-        res.status(404).send("Password doesn't match!")
-      }
+      let userData = await Doctor.update(filter, update);
+      // jwt.sign({ email: req.body.email }, function(err, token) {
+      console.log("userData ", userData);
+      // });
+      // await user.save();
+      let updatedUser = await Doctor.findOne(filter);
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).send("Password doesn't match!");
+    }
+  } else {
+    res.status(404).send("email password not match");
   }
-  else{
-    res.status(404).send('email password not match');
-  }
-})
+});
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,61 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-// var authorization = 
-let {authorization} = require('./../utils/utility');
+// var authorization =
+let { authorization } = require("./../utils/utility");
 const { Doctor } = require("../models/doctor_model");
-const posts = [{
-  userName:'nabaraj',
-  title:'book1'
-},{
-  userName:'anurag',
-  title:'book2'
-},{
-  userName:'parmita',
-  title:'book3'
-}]
+const posts = [
+  {
+    userName: "nabaraj",
+    title: "book1",
+  },
+  {
+    userName: "anurag",
+    title: "book2",
+  },
+  {
+    userName: "parmita",
+    title: "book3",
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send({test:'index'})
+  res.send({ test: "index" });
 });
-router.get('/profile', authorization, function(req, res, next) {
+router.post("/logout", authorization, async (req, res) => {
+  let userid = req.body.userId;
+  let update = { token: "" };
+  let presData = await Doctor.findByIdAndUpdate(userid, update, {
+    new: true,
+  });
+  if (!presData) res.status(404).send("Error in logout");
+  res.send("Logout successful");
+});
+router.get("/profile", authorization, function (req, res, next) {
   // res.render('index', { title: 'Express' });
-  
 
-    let userName = req.user.userName
-    console.log("$$$###%%% ",userName);
-    Doctor.findOne({
-      userName: userName
-    },(err, userProfile)=>{
-      if(err){
-        res.status(404).send('profile not found');
-      }else{
+  let userName = req.user.userName;
+  // console.log("$$$###%%% ", userName);
+  Doctor.findOne(
+    {
+      userName: userName,
+    },
+    (err, userProfile) => {
+      if (err) {
+        res.status(404).send("profile not found");
+      } else {
         res.status(200).json(userProfile);
       }
-    });
-    
+    }
+  );
+
   // res.send({test:'index'})
 });
-router.get('/posts', authorization, function(req, res, next) {
+router.get("/posts", authorization, function (req, res, next) {
   // res.render('index', { title: 'Express' });
-  console.log('req ', req.user);
+  console.log("req ", req.user);
   res.send(posts);
 });
-
-
 
 module.exports = router;
